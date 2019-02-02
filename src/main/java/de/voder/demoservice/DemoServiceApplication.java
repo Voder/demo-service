bfabd8b4d4ecddc069aec8851f2912507d9513ae
Volker Aufschild <mail@volker-aufschild.de>
2019-02-02 14:53:46 +0100

    inital start.spring.io project

---
+++ src/main/java/de/voder/demoservice/DemoServiceApplication.java
@@ -0,0 +1,14 @@
+package de.voder.demoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServiceApplication.class, args);
+	}
+
+}
+
